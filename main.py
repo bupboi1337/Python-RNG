727a05591a1dfba1a543b6805cf13468db8de5c2
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-18 15:45:12 -0700

    added some small stuff
    h

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# importing stuff
 from random import seed
 from random import randint
-print("Random Numbers Generator.")
-i = input("enter anything here to randomize: ")
+# main code
+print("Random Numbers Generator. Generates numbers 1 to 10!")
+randomizer = input("enter anything here to randomize: ")
 print("Your numbers are: ")
-seed(i)
+seed(randomizer)
 # generate numbers
 for _ in range(10):
 	value = randint(0, 10)
