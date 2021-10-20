9a65bafff8af79b9317015973eca691201f5201c
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-20 15:06:08 -0700

    Added mostly developer notes.
    I added a load of developer notes telling what most stuff does, Some credits in the code, And a little thank you message at the end of the program.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+#  PYTHON-RNG
+# By Nathaniel Carman, 2021.
+
+
 # importing stuff
-from random import seed
-from random import randint
+from random import seed                                                                            # import seed
+from random import randint                                                                         # import randit
 # main code
-print("Random Numbers Generator. Generates numbers 1 to 10!")
-randomizer = input("enter anything here to randomize. the more stuff the more random it gets!: ")
+print("Random Numbers Generator. Generates numbers 1 to 10!")                                      # first line of text you see on screen
+randomizer = input("enter anything here to randomize. the more stuff the more random it gets!: ")  # ask for user input for randomizaiton algorithm
 print("Your numbers are: ")
-seed(randomizer)
+seed(randomizer)                                                                                   # get seed to start randomizing and stuff
 # generate numbers
-for _ in range(10):
+for _ in range(10):                                                                                # define the range of numbers
     value = randint(0, 10)
-    print(value)
+    print(value)                                                                                   # print randomized numbers
+    print("thank you for using this program!")                                                     # thank the end user for using this program
