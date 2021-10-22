44da188332a2d1aa6f7c2110db8df9c812f3e310
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-22 13:36:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-                                                   # This program is licenced under GPL v3.0
-                                                   # PYTHON-RNG
-                                                   # By Nathaniel Carman, 2021.
+                                               # This program is licenced under GPL v3.0
+                                               # PYTHON-RNG
+                                               # By Nathaniel Carman, 2021.
 import random
-                                                   # Main code
-print("Python RNG 1.3. By Nathaniel Carman.")      # First line of text you see on screen
-print("Version 1.4")                               # Version counter
+                                               # Main code
+print("Python RNG 1.3. By Nathaniel Carman.")  # First line of text you see on screen
+print("Version 1.4")                           # Version counter
 print("Your random number is:")
-print(random.randint(0,10))                        # Print the random number
+print(random.randint(0,10))                    # Print the random number
 print("thank you for using this program!")
