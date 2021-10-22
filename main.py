932c856b77b36b377fd498c593570093fe9690e2
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-22 11:10:14 -0700

    Made the comments look way better

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# PYTHON-RNG
-# By Nathaniel Carman, 2021.
-
-
-# Importing Libraries
+                                                                         # PYTHON-RNG
+                                                                         # By Nathaniel Carman, 2021.
+                                                                         # Importing Libraries
 import random
-# Main code
+                                                                         # Main code
 print("Random Number Generator. Generates a number inbetween 1 and 10!") # First line of text you see on screen
-print(random.randint(0,10)) # Get and print the random number
+print(random.randint(0,10))                                              # Get and print the random number
 print("thank you for using this program!")
