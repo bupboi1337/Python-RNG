f0f695ce08a9085d2cd54fc912afe943fc0b11bd
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-22 11:07:57 -0700

    Dod some stuff with the comments
    I got rid of a comment and added another comment somewhere else

--- main.py
+++ main.py
@@ -6,5 +6,5 @@
 import random
 # Main code
 print("Random Number Generator. Generates a number inbetween 1 and 10!") # First line of text you see on screen
-print(random.randint(0,10))
-print("thank you for using this program!") # Thank the End User for using this program
+print(random.randint(0,10)) # Get and print the random number
+print("thank you for using this program!")
