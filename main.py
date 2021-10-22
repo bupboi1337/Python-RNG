a804dd18f6eff6269430374ac4d6d790ca40ab71
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-22 11:13:42 -0700

    Changed some stuff on line 6

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
                                                                          # Importing Libraries
 import random
                                                                          # Main code
-print("Random Number Generator. Generates a number inbetween 1 and 10!") # First line of text you see on screen
+print("Python RNG 1.3. By Nathaniel Carman.")                            # First line of text you see on screen
 print(random.randint(0,10))                                              # Get and print the random number
 print("thank you for using this program!")
