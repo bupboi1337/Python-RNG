038d554e7dac0101d634e5faf295793bb3d93fae
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-22 10:43:32 -0700

    Changed the way the program randomizes.
    Optimized the code to hell and back, Changed the algorithm that makes the random number, Got rid of the need for End User Input.

--- main.py
+++ main.py
@@ -3,15 +3,8 @@
 
 
 # Importing Librarys
-from random import seed
-from random import randint
+import random
 # Main code
-print("Random Numbers Generator. Generates numbers 1 to 10!")                                      # First line of text you see on screen
-randomizer = input("enter anything here to randomize. the more stuff the more random it gets!: ")  # Ask for user input for randomizaiton algorithm
-print("Your numbers are: ")
-# Generate results
-seed(randomizer)                                                                                   # Get seed to start randomizing
-for _ in range(10):                                                                                # Define the range of numbers
-    value = randint(0, 10)
-    print(value)                                                                                   # Print randomized numbers
-    print("thank you for using this program!")                                                     # Thank the End User for using this program
+print("Random Numbers Generator. Generates numbers 1 to 10!") # First line of text you see on screen
+print(random.randint(0,10))
+print("thank you for using this program!") # Thank the End User for using this program
