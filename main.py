f8973829557d2915629a95b57d7501aa9d232d3a
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-22 12:39:07 -0700

    got rid of extra spaces in the comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-                                                                         # PYTHON-RNG
-                                                                         # By Nathaniel Carman, 2021.
-                                                                         # Importing Libraries
+                                                   # PYTHON-RNG
+                                                   # By Nathaniel Carman, 2021.
+                                                   # Importing Libraries
 import random
-                                                                         # Main code
-print("Python RNG 1.3. By Nathaniel Carman.")                            # First line of text you see on screen
-print(random.randint(0,10))                                              # Get and print the random number
+                                                   # Main code
+print("Python RNG 1.3. By Nathaniel Carman.")      # First line of text you see on screen
+print(random.randint(0,10))                        # Get and print the random number
 print("thank you for using this program!")
