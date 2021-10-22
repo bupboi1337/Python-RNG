a7bf4682d4f8f9ac63c4d2645f0b9cda910c605b
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-22 10:36:03 -0700

    Updated some of the comments
    Got rid of some useless comments, Added capitalization, And added more clarity.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-#  PYTHON-RNG
+# PYTHON-RNG
 # By Nathaniel Carman, 2021.
 
 
-# importing stuff
-from random import seed                                                                            # import seed
-from random import randint                                                                         # import randit
-# main code
-print("Random Numbers Generator. Generates numbers 1 to 10!")                                      # first line of text you see on screen
-randomizer = input("enter anything here to randomize. the more stuff the more random it gets!: ")  # ask for user input for randomizaiton algorithm
+# Importing Librarys
+from random import seed
+from random import randint
+# Main code
+print("Random Numbers Generator. Generates numbers 1 to 10!")                                      # First line of text you see on screen
+randomizer = input("enter anything here to randomize. the more stuff the more random it gets!: ")  # Ask for user input for randomizaiton algorithm
 print("Your numbers are: ")
-seed(randomizer)                                                                                   # get seed to start randomizing and stuff
-# generate numbers
-for _ in range(10):                                                                                # define the range of numbers
+# Generate results
+seed(randomizer)                                                                                   # Get seed to start randomizing
+for _ in range(10):                                                                                # Define the range of numbers
     value = randint(0, 10)
-    print(value)                                                                                   # print randomized numbers
-    print("thank you for using this program!")                                                     # thank the end user for using this program
+    print(value)                                                                                   # Print randomized numbers
+    print("thank you for using this program!")                                                     # Thank the End User for using this program
