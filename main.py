e6322728d4164f735b10966b6b499c811b205a0e
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-26 09:25:48 -0700

    added moar comments

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
  # PYTHON-RNG
  # By Nathaniel Carman, 2021.
 import random
- # define stuff
+ # define variables
 v1 = 1
 v2 = 4
 v3 = 2
  # Main code
 print("Python RNG, By Nathaniel Carman.") # First line of text you see on screen
-print("Verison", v1, ".", v2, ".", v3)
+print("Verison", v1, ".", v2, ".", v3) # Print version number
 print("Your number is:")
 print(random.randint(0,10)) # Print the number
